33b30469da260cf34d261c5dcdeaed8d561452c4
Kohmmz <jeremiahkoome1@gmail.com>
2025-02-01 22:11:32 +0300

    Implement user fetching and context sharing across components

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,27 +1,19 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+// UserProfile.js
+import { useParams, useOutletContext } from "react-router-dom";
 
 function UserProfile() {
-  const [user, setUser] = useState({});
-  const params = useParams();
-  const userId = params.id;
+  const { id } = useParams();
+  const users = useOutletContext();
+  const user = users.find((user) => user.id === parseInt(id));
 
-  useEffect(() =>{
-    fetch(`http://localhost:4000/users/${userId}`)
-    .then(r => r.json())
-    .then(data => setUser(data))
-    .catch(error => console.error(error));
-  }, [userId]);
+  if (!user) return <h2>User not found</h2>;
 
-  if(!user.name){
-    return <h1>Loading...</h1>;
-  };
-
-  return(  
-      <aside>
-        <h1>{user.name}</h1>
-      </aside>
+  return (
+    <section>
+      <h2>{user.name}</h2>
+      <p>Email: {user.email}</p>
+    </section>
   );
-};
+}
 
 export default UserProfile;
